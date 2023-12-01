d5b2bef47b5206c77e08ff357248fae0cb6771bd
John Kipruto <johnkipruto157@gmail.com>
2023-12-01 16:23:28 +0300

    Allow cors

--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ import SessionsController from './controllers/sessions_controller.js'
 import CustomersController from './controllers/customers_controller.js'
 import Seeder from './seeder.js'
 import session from 'express-session'
+import cors from 'cors'
 
 const app = express()
 
 connectToDB()
 
+
 app.use(express.json())
+app.use(cors());
 
 app.use(session({
     secret: 'Braka Botique secret key',
