a889711e63293074fe4fdce1d093c07009a0d71f
John Kipruto <johnkipruto157@gmail.com>
2023-12-01 16:30:56 +0300

    Allow cors

--- server/index.js
+++ server/index.js
@@ -14,11 +14,7 @@ connectToDB()
 
 app.use(express.json())
 app.use(cors());
-app.use(cors({
-    origin: "https://baraka-boutique-chepseon.vercel.app/"
-}
-    
-));
+
 
 app.use(session({
     secret: 'Braka Botique secret key',
