2b791648bf223d7ad4f6b6a7b65450ba16377f39
John Kipruto <johnkipruto157@gmail.com>
2023-12-04 18:31:29 +0300

    Allow cors for multiple urls

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ const store = new mongoDBStore({
     uri: connURI,
     collection: "userSessions"
 })
+app.use(cors())
 app.use(cors({
     credentials: true,
     origin: ["http://localhost:3000", "https://baraka-boutique-chepseon.vercel.app/"]
