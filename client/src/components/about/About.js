495a11146a4993809e675cddf9714993108ff1ac
John Kipruto <johnkipruto157@gmail.com>
2024-03-08 11:14:31 +0300

    Update team member name

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -13,7 +13,7 @@ const About = () => {
               <div className='team-member'>
                 <div className='image-container'><img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' /></div>
                 <div>
-                  <h5>Jane Doe</h5>
+                  <h5>Janey</h5>
                   <p>Founder</p>
                 </div>
               </div>
