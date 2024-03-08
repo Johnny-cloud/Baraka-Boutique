795b8a8146fa05d6aff2dd8e022cd380db4ae27a
John Kipruto <johnkipruto157@gmail.com>
2024-03-08 10:14:36 +0300

    Update the contact page

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -5,6 +5,27 @@ const Contact = () => {
   return (
     <div className='contact'>
         <TopLanding title={'Contact'} />
+        <div className='main-content'>
+        <h4>GET IN TOUCH</h4>
+        <form>
+          <div>
+            <label htmlFor='name'>Your name</label>
+            <input type='text' id='name' />
+          </div>
+          <div>
+            <label htmlFor='email'>Your email</label>
+            <input type='email' id='email' />
+          </div>
+          <div>
+            <label htmlFor='meassage'>Your message</label>
+            <textarea type='text' id='message'/>
+          </div>
+          <div>
+            <button>Send Message</button>
+          </div>
+        </form>
+        </div>
+        
     </div>
   )
 }
